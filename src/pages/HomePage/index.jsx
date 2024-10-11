a2ba4a1ca22d0b9b057944e5510a8f57b3001cfb
GabrielNunex <gabrielnunes.design@gmail.com>
2024-10-11 09:47:40 -0300

    Entrega 11/10

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,11 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { CartModal } from "../../components/CartModal";
 import { Header } from "../../components/Header";
 import { ProductList } from "../../components/ProductList";
 
 export const HomePage = () => {
    const [productList, setProductList] = useState([]);
-   const [cartList, setCartList] = useState([]);
+   const localStorageCartList = localStorage.getItem("@CARTLIST");
+   const [cartList, setCartList] = useState(localStorageCartList ? JSON.parse(localStorageCartList) : []);
+   const [isOpen, setIsOpen] = useState(false);
+   
+   const addCart = (item) => {
+      if(cartList.some(inCart => inCart.id  === item.id)) {
+         alert("Item já adicionado")
+      } else {
+         setCartList([...cartList, item])
+      }
+   }
+
+   const removeCart = (itemId) => {
+      const newCart = cartList.filter(item => item.id !== itemId);
+      setCartList(newCart);
+   }
+
+   const cleanCart = () => {
+      const cart = [];
+      setCartList(cart);
+   }
+
+   useEffect(() => {
+      const getList = async () => {
+         const response = await fetch("https://hamburgueria-kenzie-json-serve.herokuapp.com/products");
+         const json = await response.json();
+         setProductList(json);
+      }
+      getList();
+   }, [])
+
+   useEffect (() => {
+      localStorage.setItem("@CARTLIST", JSON.stringify(cartList))
+   }, [cartList])
+
 
    // useEffect montagem - carrega os produtos da API e joga em productList
    // useEffect atualização - salva os produtos no localStorage (carregar no estado)
@@ -16,10 +50,10 @@ export const HomePage = () => {
 
    return (
       <>
-         <Header />
+         <Header cartList={cartList} setIsOpen={setIsOpen}/>
          <main>
-            <ProductList productList={productList} />
-            <CartModal cartList={cartList} />
+            <ProductList productList={productList} addCart={addCart}/>
+            {isOpen ? <CartModal cartList={cartList} setIsOpen={setIsOpen} removeCart={removeCart} cleanCart={cleanCart}/> : null}
          </main>
       </>
    );
